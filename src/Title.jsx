39554155eba3e1b875e2c5899cf6e2955d7dc451
Muhammad Firdaus <mbex71@gmail.com>
2021-11-10 18:48:44 +0700

    adding props to component

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Title = () => {
-  return <h1>Web 2 Cok</h1>;
+const Title = (props) => {
+  return <h1>Web 2 Cok {props.title ? props.title : null}</h1>;
 };
 
 export default Title;
