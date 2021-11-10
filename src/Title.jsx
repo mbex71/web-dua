fe4845e8ded280d8ef19f4596a645e70c416240b
Muhammad Firdaus <mbex71@gmail.com>
2021-11-10 18:56:09 +0700

    change wording test

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Title = (props) => {
-  return <h1>Web 2 Cok {props.title ? props.title : null}</h1>;
+  return <h1>Web testing untuk {props.title ? props.title : null}</h1>;
 };
 
 export default Title;
